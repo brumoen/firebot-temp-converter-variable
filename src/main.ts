c09657e5fa10c2886a3f55f3a5f07238a9d14510
Perry <brumoen@gmail.com>
2021-11-23 15:29:43 +0100

    Added more examples and changed scale to unit.

--- src/main.ts
+++ src/main.ts
@@ -22,27 +22,39 @@ const script: Firebot.CustomScript<Params> = {
     replaceVariableManager.registerReplaceVariable({
       definition: {
         handle: "tempConverter",
-        description: "Converts the given temperatur to the assigned scale.",
-        usage: "tempConvert[num, scale]",
+        description: "Converts the given temperatur based on unit, if you don't assign a unit Celsius(C) is assumed.",
+        usage: "tempConvert[num, unit]",
         examples: [
           {
-            usage: "tempConverter[25, F]",
+            usage: "tempConverter[25, C]",
+            description: "Converts 25°C to Fahrenheit"
+          },
+          {
+            usage: "tempConverter[77, F]",
+            description: "Converts 77°F to Celsius"
+          },
+          {
+            usage: "arg[1]°$arg[2] converts to $tempConverter[$arg[1], $arg[2]]",
+            description: "This takes argument 1 as tempteratur and argument 2 as unit and converts it."
+          },
+          {
+            usage: "tempConverter[25]",
             description: "Converts 25°C to Fahrenheit"
           }
         ],
         possibleDataOutput: ["text"],
       },
-      evaluator: (_, temp, scale = "F") => {
+      evaluator: (_, temp, unit = "C") => {
 
         if (isNaN(temp)) {
           return 0;
         }
 
-        if (scale === "C"){
+        if (unit === "C"){
           temp = (temp*1.8)+32;
           temp = Number(temp).toFixed(2);
           temp = temp + "°F";
-        } else if (scale === "F"){
+        } else if (unit === "F"){
           temp = (temp-32)/1.8;
           temp = Number(temp).toFixed(2);
           temp = temp + "°C"
